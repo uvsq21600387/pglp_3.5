c6a7e58c8030a46c9e85378f04ffd88bb06c50ee
Mathieu De Jesus <mathieu.de-jesus@ens.uvsq.fr>
2020-02-28 21:15:49 +0100

    maven repository

---
+++ pglp_3_5/src/main/java/Mathieu_De_Jesus/pglp_3_5/App.java
@@ -0,0 +1,13 @@
+package Mathieu_De_Jesus.pglp_3_5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
